3fdd77a7ac318c5182725b575c9affd12594af81
jayshao830413 <shaoshijie830413@foxmail.com>
2012-10-15 00:23:09 +0800

    for learning bootstrap and entity framework base project

---
+++ University_Demo/DAL/UniversityContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using University_Demo.Models;
+using System.Web;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace University_Demo.DAL
+{
+    public class UniversityContext : DbContext
+    {
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ University_Demo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="University_Demo.MvcApplication" Language="C#" %>
